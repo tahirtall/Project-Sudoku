91abecbd99a12ea803281fad8a6ed5b466af96f6
Tahir Uzun <tahirtall46@gmail.com>
2020-03-05 17:28:06 -0600

    step 1 complete

--- sudoku.java
+++ sudoku.java
@@ -1,37 +1,31 @@
 import java.util.Arrays;
+import java.util.ArrayList;
 
 class sudoku
 {
 	public static void main(String[] args)
 	{
-		int[][] board = new int[3][3];
-		// adding elements to the board.
-		board[0][0] = 5;
-		board[0][1] = 0;
-		board[0][2] = 0;
-
-		board[1][0] = 7;
-		board[1][1] = 9;
-		board[1][2] = 0;
-
-		board[2][0] = 2;
-		board[2][1] = 0;
-		board[2][2] = 4;
+		// board array initialized.
+		String[][] board = {{"5", "0", "0"}, {"7", "9", "0"}, {"2", "0", "4"}};
+		String[][] newBoard = new String[3][3];
 
 		// Iterating through the grid.
-		for (int row=0; row<3; row++)
+		for (int i=0; i<board.length; i++)
 		{
-			for (int col=0; col<board[row].length; col++)
+			for (int j=0; j<board[i].length; j++)
 			{
-				if (board[col].equals(0))
+				if (board[i][j] == "0")
 				{
-					board[col] = board[1];
+					newBoard[i][j] = "1";
+				}
+				else {
+					newBoard[i][j] = board[i][j];
 				}
 			}
 		}
 		// Printing the array items
-		System.out.print("[ " + board[0][0] + " " + board[0][1] + " " + board[0][2] + " \n" +
-						 "  " +	board[1][0] + " " + board[1][1] + " " + board[1][2] + " \n" +
-						 "  " +	board[2][0] + " " + board[2][1] + " " + board[2][2] + " ]");
+		System.out.print("[ " + newBoard[0][0] + " " + newBoard[0][1] + " " + newBoard[0][2] + " \n" +
+						 "  " +	newBoard[1][0] + " " + newBoard[1][1] + " " + newBoard[1][2] + " \n" +
+						 "  " +	newBoard[2][0] + " " + newBoard[2][1] + " " + newBoard[2][2] + " ]");
 		}
 }
