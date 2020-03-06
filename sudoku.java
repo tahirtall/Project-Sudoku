a1ec9473151a8398775a3555883860c3eeacd291
Tahir Uzun <tahirtall46@gmail.com>
2020-03-05 19:25:36 -0600

    not prints to look like a grid.

--- sudoku.java
+++ sudoku.java
@@ -1,19 +1,16 @@
-import java.util.Arrays;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 class sudoku {
 	public static void main(String[] args) throws IOException
 	{
-		List<List<Integer>> integers = new ArrayList<List<Integer>>(9);
+		List<List<Integer>> board = new ArrayList<List<Integer>>(9);
 		for (int i =0; i<9; i++) {
-			integers.add(new ArrayList<Integer>(9));
+			board.add(new ArrayList<Integer>(9));
 		}
 		Path filePath = Paths.get("numbers.txt");
 		Scanner scanner = new Scanner(filePath);
@@ -21,12 +18,14 @@ class sudoku {
 		int counter = 0;
 		while (num < 9) {
 			while (counter < 9) {
-					integers.get(num).add(scanner.nextInt());
+					board.get(num).add(scanner.nextInt());
 					counter++;
 			}
 			num++;
 			counter = 0;
 		}
-		System.out.print(integers);
+		for (int i=0; i<9; i++) {
+			System.out.println(board.get(i));
+		}
 	}
 }
