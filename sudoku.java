4fb105a3fee8c0e5233b8439e08c8a7c67149eac
Tahir Uzun <tahirtall46@gmail.com>
2020-03-07 00:43:27 -0600

    instead of reading from the file, now the table is decleared by a global variable inside the sudoku.java

--- sudoku.java
+++ sudoku.java
@@ -4,38 +4,42 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
-class sudoku {
-
-    private final static int MIN_VALUE = 0;
-    private final static int MAX_VALUE = 9;
-    private final static int BOARD_START_INDEX = 0;
-    private final static int BOARD_SIZE = 9;
-    private final static int SUBSECTION = 3;
-    private final static int NO_VALUE = 0;
-
-    private static List<List<Integer>> board = prepTheGame();
+public class sudoku {
     
+    // 0s represent blank spaces
+    private static int[][] board = {
+        {8, 0, 7, 1, 5, 0, 0, 9, 6},
+        {0, 6, 5, 3, 0, 7, 1, 4, 0},
+        {3, 4, 1, 0, 8, 0, 7, 0, 2},
+        {5, 9, 3, 4, 6, 8, 2, 7, 0},
+        {4, 0, 0, 0, 1, 0, 0, 0, 9},
+        {0, 1, 8, 9, 7, 2, 4, 3, 5},
+        {7, 0, 6, 0, 3, 0, 9, 1, 4},
+        {0, 5, 4, 7, 0, 6, 8, 2, 0},
+        {2, 3, 0, 0, 4, 1, 5, 0, 7}
+    };
 
-	// Returns 2D Array of the Answer
-	public static List<List<Integer>> showTheAnswer() throws IOException {
-		List<List<Integer>> board = new ArrayList<List<Integer>>(9);
-		for (int i=0; i<9; i++) {
-			board.add(new ArrayList<Integer>(9));
-		}
-		Path filePath = Paths.get("solved.txt");
-		Scanner scanner = new Scanner(filePath);
-		int num = 0;
-		int counter = 0;
-		while (num < 9) {
-			while (counter < 9) {
-                board.get(num).add(scanner.nextInt());
-				counter++;
-			}
-			num++;
-			counter = 0;
-		}
-		return board;
+    private static int[][] solvedBoard = {
+        {8, 2, 7, 1, 5, 4, 3, 9, 6},
+        {9, 6, 5, 3, 2, 7, 1, 4, 8},
+        {3, 4, 1, 6, 8, 9, 7, 5, 2},
+        {5, 9, 3, 4, 6, 8, 2, 7, 1},
+        {4, 7, 2, 5, 1, 3, 6, 8, 9},
+        {6, 1, 8, 9, 7, 2, 4, 3, 5},
+        {6, 1, 8, 9, 7, 2, 4, 3, 5},
+        {1, 5, 4, 7, 9, 6, 8, 2, 3},
+        {2, 3, 9, 8, 4, 1, 5, 6, 7}
+    };
+
+    // Returns 2D Array of the Answer
+	public void showTheAnswer() {
+        for (int i=0; i<9; i++) {
+            for(int j=0; j=9; j++) {
+                System.out.print(solvedBoard[i][j] + " ");
+            }
+        }
 	}
 
 	// Reads in the txt file, and creates a 2D Arraylist.
@@ -59,35 +63,22 @@ class sudoku {
 		return board;
 	}
 
-	// Returns index of the column and the row where 0 exists in the board.
-	public static List<List<Integer>> indexMeZero(List<List<Integer>> s){
-		int counter = -1;
-		int counter2 = -1;
-		List<List<Integer>> index = new ArrayList<List<Integer>>(9);
-		for (int i=0; i<9; i++) {
-			index.add(new ArrayList<Integer>());
-		}
-		for (int row=0; row<9; row++) {
-			counter2++;
-			if (s.get(row).contains(0)) {
-				for(int col=0; col<9; col++) {
-					counter++;
-					if (s.get(row).get(col) == 0) {
-						index.get(counter2).add(counter);
-					}
-				}
-			}
-			counter = -1;
-		}
-		return index;
-	}
-    
-    // Checks the row if the curren
+    // Row constraints
     public static boolean checkRow(){
-        
+        // TODO
         return true;
+    }
 
-
+    // Column constraints
+    public static boolean checkColumn() {
+        // TODO
+        return true;
+    }
+    
+    // Checks if row and column constraints clear out.
+    public static boolean isValid() {
+        // TODO
+        return true;
     }
 
 
@@ -97,43 +88,12 @@ class sudoku {
 		List<List<Integer>> index = new ArrayList<List<Integer>>(9);
 		List<List<Integer>> board = new ArrayList<List<Integer>>(9);
 		board = prepTheGame();
-		index = indexMeZero(board);
 		// TODO: Use index object to somehow check rows and columns and somehow edit down the correct number and return it.
 		return board;
 	}
 
 	public static void main(String[] args) throws IOException
 	{
-		List<List<Integer>> input = new ArrayList<List<Integer>>();
-		List<List<Integer>> output = new ArrayList<List<Integer>>(9);
-		List<List<Integer>> expected = new ArrayList<List<Integer>>(9);
-
-		input = prepTheGame();
-		output = playTheGame();
-		expected = showTheAnswer();
-
-		// Indexes of the 0s.
-		System.out.println("\nIndexes of 0s:");
-		System.out.println(indexMeZero(input));
-		// Below prints input, output, and expected sudoku grids.
-		System.out.println("Input:");
-		System.out.print("[" + input.get(0) + "\n");
-		for (int i=1; i<8; i++) {
-			System.out.print(" " + input.get(i) + "\n");
-		}
-		System.out.print(" " + input.get(8) + "]\n");
-		System.out.println("Output:");
-		System.out.print("[" + output.get(0) + "\n");
-		for (int i=1; i<8; i++) {
-			System.out.print(" " + output.get(i) + "\n");
-		}
-		System.out.print(" " + output.get(8) + "]\n");
-		System.out.println("Expected:");
-		System.out.print("[" + expected.get(0) + "\n");
-		for (int i=1; i<8; i++) {
-			System.out.print(" " + expected.get(i) + "\n");
-		}
-		System.out.print(" " + expected.get(8) + "]\n");
-		System.out.print(" ");
+        System.out.print(showTheAnswer());
 	} 
 }
